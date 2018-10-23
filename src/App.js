ae23e5a162749095d3e504037e452623f77e74cb
mikehatch <mike.hatch@outlook.com>
2018-10-23 16:39:43 -0700

    Added basic router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './music-note.svg';
 import './App.css';
 import {slide as Menu} from 'react-burger-menu';
+import { Link } from 'react-router-dom';
+
 
 class App extends Component {
 
@@ -13,11 +15,13 @@ class App extends Component {
     return (
       <div className="App">
         <Menu>
-          <a id="home" className="menu-item" href="/">Home</a>
-          <a id="mysongs" className="menu-item" href="/mysongs">My Songs</a>
-          {/* <a id="about" className="menu-item" href="/about">About</a>
-          <a id="contact" className="menu-item" href="/contact">Contact</a> */}
-          {/* <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
+            {/* <a id="home" className="menu-item" href="/">Home</a> */}
+            <Link to="/" className="menu-item">Home</Link> 
+            <Link to="/mysongs" className="menu-item">My Songs</Link>
+            {/* <a id="mysongs" className="menu-item" href="/mysongs">My Songs</a> */}
+            {/* <a id="about" className="menu-item" href="/about">About</a>
+            <a id="contact" className="menu-item" href="/contact">Contact</a> */}
+            {/* <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
         </Menu>
         <main >
           <header className="App-header">
