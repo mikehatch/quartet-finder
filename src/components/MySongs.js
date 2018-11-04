b3a26b7aed06b6d5eab0f0a19652be0ca371080f
mikehatch <mike.hatch@outlook.com>
2018-11-03 23:44:50 -0700

    Added service call to mysong (not functional)

--- src/components/MySongs.js
+++ src/components/MySongs.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
  class MySongs extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      songs: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://www.instantquartet.org/rest/findquartet?authToken=fA4HLTIa0yCMkVbtvnz4&s1=Mike%20Hatch')
+      .then(response => response.json())
+      .then(data => this.setState({ songs: data.quartets }));
+      
+  }
+  
   render() {
+    const { songs } = this.state;
+    console.log(songs);
     return (
       <div>
-        <h1  className="App-header">MySongs</h1>
+        <h1  className="App-header">My Songs</h1>
+        <ul>
+          {songs.map(song =>
+            <li key={song.title}>
+              <div>{song.title}</div>
+            </li>
+          )}
+        </ul>
       </div>
     )
   }
